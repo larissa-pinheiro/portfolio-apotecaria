fd92ad3c05b0e54eeeee305b01eb0c2ed2015a82
larissa <rissapins@gmail.com>
2025-07-23 16:02:56 -0300

    feat: adiciona validações necessárias para o formulário

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const messageTextarea = document.getElementById('message');
+  const formulario = document.getElementById('formulario');
+  const campoNome = document.getElementById('name');
+  const campoEmail = document.getElementById('email');
+  const campoSubject = document.getElementById('subject');
+  const campoMessage = document.getElementById('message');
+
+  const txtNome = document.getElementById('txtNome');
+  const txtEmail = document.getElementById('txtEmail');
+  const txtSubject = document.getElementById('txtSubject');
   const charCount = document.getElementById('char-count');
   const charCounter = document.getElementById('char-counter');
+
   const maxLength = 500;
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2, 3})+$/;
 
   function updateCharCount() {
-    const currentLength = messageTextarea.value.length;
+    const currentLength = campoMessage.value.length;
     charCount.textContent = currentLength;
 
     charCounter.classList.remove('warning', 'danger');
@@ -17,11 +27,43 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  messageTextarea.addEventListener('input', updateCharCount);
-
-  messageTextarea.addEventListener('paste', function () {
-    setTimeout(updateCharCount, 10);
-  });
-
+  campoMessage.addEventListener('input', updateCharCount);
+  campoMessage.addEventListener('paste', () => setTimeout(updateCharCount, 10));
   updateCharCount();
+
+  formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let isValid = true;
+
+    if (campoNome.value.trim().length < 3) {
+      txtNome.textContent = 'O Nome deve ter no mínimo 3 caracteres.';
+      campoNome.focus();
+      isValid = false;
+    } else {
+      txtNome.textContent = '';
+    }
+
+    if (!campoEmail.value.match(emailRegex)) {
+      txtEmail.textContent = 'Digite um E-mail válido.';
+      if (isValid) campoEmail.focus();
+      isValid = false;
+    } else {
+      txtEmail.textContent = '';
+    }
+
+    if (campoSubject.value.trim().length < 5) {
+      txtSubject.textContent = 'O Assunto deve ter no mínimo 5 caracteres.';
+      if (isValid) campoSubject.focus();
+      isValid = false;
+    } else {
+      txtSubject.textContent = '';
+    }
+
+    if (isValid) {
+      alert("Formulário Enviado com Sucesso!");
+      formulario.reset();
+      updateCharCount();
+    }
+  });
 });
